3725a6fc620e835dbcec1d00cf6b0c0f31eca022
phirom-02 <phiromkhim02@gmail.com>
2025-05-10 22:55:01 +0700

    init

---
+++ src/main/java/com/firom/arcane_aether_api/ArcaneAetherApiApplication.java
@@ -0,0 +1,13 @@
+package com.firom.arcane_aether_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcaneAetherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcaneAetherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firom/arcane_aether_api/ArcaneAetherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firom.arcane_aether_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcaneAetherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
